d4ba511f525586403835cf580d442652c7321714
Vinicius Teixeira <vinicius0026@gmail.com>
2017-11-08 13:38:22 -0200

    refactoring createRenderer to a separate file

---
+++ build/createRenderer.js
@@ -0,0 +1,26 @@
+const Fs = require('fs')
+const LRU = require('lru-cache')
+const Path = require('path')
+const { createBundleRenderer } = require('vue-server-renderer')
+
+const template = Fs.readFileSync(resolve('../assets/index.template.html'), 'utf-8')
+
+module.exports = function (bundle, options) {
+  return createBundleRenderer(bundle, {
+    ...options,
+    template,
+    // for component caching
+    cache: LRU({
+      max: 1000,
+      maxAge: 1000 * 60 * 15
+    }),
+    // this is only needed when vue-server-renderer is npm-linked
+    basedir: resolve('../public'),
+    // recommended for performance
+    runInNewContext: false
+  })
+}
+
+function resolve (file) {
+  return Path.resolve(__dirname, file)
+}
--- plugins/appServer.js
+++ plugins/appServer.js
@@ -1,10 +1,4 @@
-const Fs = require('fs')
-const LRU = require('lru-cache')
-const Path = require('path')
-
-const { createBundleRenderer } = require('vue-server-renderer')
-
-const template = Fs.readFileSync(resolve('../assets/index.template.html'), 'utf-8')
+const createRenderer = require('../build/createRenderer')
 
 const bundle = require('../public/vue-ssr-server-bundle.json')
 const clientManifest = require('../public/vue-ssr-client-manifest.json')
@@ -38,24 +32,4 @@ function registerRoutes (server) {
       }
     }
   })
-}
-
-function createRenderer (bundle, options) {
-  return createBundleRenderer(bundle, {
-    ...options,
-    template,
-    // for component caching
-    cache: LRU({
-      max: 1000,
-      maxAge: 1000 * 60 * 15
-    }),
-    // this is only needed when vue-server-renderer is npm-linked
-    basedir: resolve('../public'),
-    // recommended for performance
-    runInNewContext: false
-  })
-}
-
-function resolve (file) {
-  return Path.resolve(__dirname, file)
 }
