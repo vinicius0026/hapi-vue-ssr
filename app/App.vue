18a929452705b9ac957566b98244b3a3586a4a9a
Vinicius Teixeira <vinicius0026@gmail.com>
2017-11-15 19:30:43 -0200

    removing home link from top nav, fixing link in logo placeholder

--- app/App.vue
+++ app/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <b-navbar toggleable="md" type="dark" variant="info">
       <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-      <b-navbar-brand href="#">Navbar</b-navbar-brand>
+      <b-navbar-brand to="/">LOGO</b-navbar-brand>
       <b-collapse is-nav id="nav_collapse">
         <b-navbar-nav>
           <b-nav-item v-for="(link, i) in links"
@@ -26,7 +26,6 @@ export default {
   data () {
     return {
       links: [
-        { icon: 'apps', title: 'Home', to: '/' },
         { icon: 'shopping_basket', title: 'Products', to: '/products' },
         { icon: 'bubble_chart', title: 'About', to: '/about' }
       ]
